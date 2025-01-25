a16085c642b528119fb2641d972160b4fb94dcfe
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-25 21:02:04 +0200

    refactor; remove unused import

--- src/main/java/com/nasser/urlfeederservice/model/URL.java
+++ src/main/java/com/nasser/urlfeederservice/model/URL.java
@@ -1,6 +1,6 @@
 package com.nasser.urlfeederservice.model;
 
-import com.fasterxml.jackson.annotation.JsonAnySetter;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
