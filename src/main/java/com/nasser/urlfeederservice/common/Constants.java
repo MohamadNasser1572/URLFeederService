63f5e2ba0a58a916884c3a0ed211bf0f77cb6a35
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-25 19:32:36 +0200

    feat: add URL class with its instances

---
+++ src/main/java/com/nasser/urlfeederservice/common/Constants.java
@@ -0,0 +1,2 @@
+package com.nasser.urlfeederservice.common;public class Constants {
+}
--- src/main/java/com/nasser/urlfeederservice/model/URL.java
+++ src/main/java/com/nasser/urlfeederservice/model/URL.java
@@ -1,2 +1,26 @@
-package com.nasser.urlfeederservice.model;public class URL {
+package com.nasser.urlfeederservice.model;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class URL {
+    @Id
+    String id;
+
+    String url;
+    @Column(name = "created_date")
+    Timestamp createdDate;
 }
