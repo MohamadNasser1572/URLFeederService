a52d032fe045d6d9649deb6a106f99a4438158cc
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-25 21:00:28 +0200

    feat: add createdDate annotation

--- src/main/java/com/nasser/urlfeederservice/model/URL.java
+++ src/main/java/com/nasser/urlfeederservice/model/URL.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.sql.Timestamp;
 
@@ -16,11 +18,14 @@ import java.sql.Timestamp;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "url")
 public class URL {
     @Id
     String id;
 
     String url;
+
+    @CreatedDate
     @Column(name = "created_date")
     Timestamp createdDate;
 }
