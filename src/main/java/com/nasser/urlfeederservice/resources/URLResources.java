aa0ca2edce747b7fdbb8082018ce35b59548467a
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-25 21:03:44 +0200

    refactor: remove unused import

--- src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
+++ src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.Timestamp;
 import java.util.UUID;
 
 @RestController
