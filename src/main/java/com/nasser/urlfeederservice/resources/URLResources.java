34f5a0e2f860ef9d471988e1953a2292882ea8c2
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-25 19:33:42 +0200

    feat: add UUID URL for better URL String visual

--- src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
+++ src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
@@ -1,21 +1,27 @@
 package com.nasser.urlfeederservice.resources;
 
+import com.nasser.urlfeederservice.common.Constants;
+import com.nasser.urlfeederservice.model.URL;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class URLResources {
+
     @GetMapping("/ping")
-    public String ping(){
+    public String ping() {
         return "pong";
     }
 
     //Create an endpoint that posts a String url that can return it on the compiler
     @PostMapping
-    public ResponseEntity<Void> submitURL(@RequestBody String url){
+    public ResponseEntity<Void> submitURL(@RequestBody URL url) {
+        url.setId(Constants.URL_UUID_PREFIX + UUID.randomUUID().toString());
         System.out.println(url);
         return ResponseEntity.ok().build();
     }
