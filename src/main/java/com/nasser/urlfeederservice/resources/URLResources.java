9b9d68840a17311cf9623a56923eec712373f532
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-25 19:57:18 +0200

    feat: set the date + save

--- src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
+++ src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
@@ -2,16 +2,23 @@ package com.nasser.urlfeederservice.resources;
 
 import com.nasser.urlfeederservice.common.Constants;
 import com.nasser.urlfeederservice.model.URL;
+import com.nasser.urlfeederservice.service.URLService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @RestController
 public class URLResources {
+
+    @Autowired
+    private URLService urlService;
+
 
     @GetMapping("/ping")
     public String ping() {
@@ -22,6 +29,8 @@ public class URLResources {
     @PostMapping
     public ResponseEntity<Void> submitURL(@RequestBody URL url) {
         url.setId(Constants.URL_UUID_PREFIX + UUID.randomUUID().toString());
+        url.setCreatedDate(new Timestamp(System.currentTimeMillis()));//setting the date
+        urlService.save(url);//saving
         System.out.println(url);
         return ResponseEntity.ok().build();
     }
