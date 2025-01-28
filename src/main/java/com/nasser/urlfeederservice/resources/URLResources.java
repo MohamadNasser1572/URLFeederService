1c54bd82bd6b7115b23ceefdfb53350f96713a02
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-28 12:38:56 +0200

    feat: set created date upon submitting url

--- src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
+++ src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @RestController
@@ -27,6 +29,7 @@ public class URLResources {
     @PostMapping
     public ResponseEntity<Void> submitURL(@RequestBody URL url) {
         url.setId(Constants.URL_UUID_PREFIX + UUID.randomUUID().toString());
+        url.setCreatedDate(new Timestamp(System.currentTimeMillis()));
         urlService.save(url);//saving
         System.out.println(url);
         return ResponseEntity.ok().build();
