f5e9e515dad4ea7d9aa3f7c10b5b8b941e4d773f
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-30 23:43:15 +0200

    refactor: add kafka service for sending on html

--- src/main/java/com/nasser/urlfeederservice/service/URLService.java
+++ src/main/java/com/nasser/urlfeederservice/service/URLService.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class URLService {
     @Autowired
+    private KafkaService kafkaService;
+
+    @Autowired
     private URLRepository urlRepository;
 
     public void save(URL url) {
         urlRepository.save(url);
+        kafkaService.send("html_topic", url.getUrl());
     }
 }
