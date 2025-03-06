fa9103fb9636c4ee802bdfc6054d2ff54a5e1342
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-03-06 12:48:56 +0200

    refactor: add comment to kafkaservice

--- src/main/java/com/nasser/urlfeederservice/service/KafkaService.java
+++ src/main/java/com/nasser/urlfeederservice/service/KafkaService.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+//This is a service that sends URLs to kafka topic. This service uses KafKaTemplate to publish messages.
 @Service
 public class KafkaService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String topic, String message){
+    public void send(String topic, String message) {
         kafkaTemplate.send(topic, message);
     }
 }
