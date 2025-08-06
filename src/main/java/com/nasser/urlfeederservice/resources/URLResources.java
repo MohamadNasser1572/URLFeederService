a92c1d9a0363de68a0a5885e791df1057585cc7a
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-08-06 08:33:14 +0300

    update: add saving url

--- src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
+++ src/main/java/com/nasser/urlfeederservice/resources/URLResources.java
@@ -5,10 +5,7 @@ import com.nasser.urlfeederservice.model.URL;
 import com.nasser.urlfeederservice.service.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -19,18 +16,17 @@ public class URLResources {
     @Autowired
     private URLService urlService;
 
-
+    //TEST TEST TEST
     @GetMapping("/ping")
     public String ping() {
         return "pong";
     }
-
     //Create an endpoint that posts a String url that can return it on the compiler
     @PostMapping
     public ResponseEntity<Void> submitURL(@RequestBody URL url) {
         url.setId(Constants.URL_UUID_PREFIX + UUID.randomUUID().toString());
         url.setCreatedDate(new Timestamp(System.currentTimeMillis()));
-        urlService.save(url);//saving
+        urlService.save(url);
         System.out.println(url);
         return ResponseEntity.ok().build();
     }
