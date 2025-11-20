f974eadd4ee677d88de606bfe9a5bb3785ebd60b
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-11-20 09:50:56 +0200

    update: remove redundant annotations

--- src/main/java/com/nasser/urlfeederservice/UrlFeederServiceApplication.java
+++ src/main/java/com/nasser/urlfeederservice/UrlFeederServiceApplication.java
@@ -1,19 +1,14 @@
 package com.nasser.urlfeederservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = "com.nasser.urlfeederservice.dao")
 public class UrlFeederServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlFeederServiceApplication.class, args);
 	}
-
 }
